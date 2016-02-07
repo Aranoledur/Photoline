e3c554c5875fc70044d09b43940931270f40da86
Nikolay Ischuk <ischuknn@gmail.com>
2016-02-07 20:31:08 +0300

    some optimization more colors button selectable

--- Makestagram/Helpers/UIImage+AvarageColor.swift
+++ Makestagram/Helpers/UIImage+AvarageColor.swift
@@ -12,6 +12,8 @@ extension UIImage {
     func areaAverage() -> UIColor {
         var bitmap = [UInt8](count: 4, repeatedValue: 0)
         
+        //too slow maybe later
+        /*
         if #available(iOS 9.0, *) {
             // Get average color.
             let context = CIContext()
@@ -25,14 +27,14 @@ extension UIImage {
             
             // Render to bitmap.
             context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: kCIFormatRGBA8, colorSpace: CGColorSpaceCreateDeviceRGB())
-        } else {
-            // Create 1x1 context that interpolates pixels when drawing to it.
-            let context = CGBitmapContextCreate(&bitmap, 1, 1, 8, 4, CGColorSpaceCreateDeviceRGB(), CGBitmapInfo.ByteOrderDefault.rawValue | CGImageAlphaInfo.PremultipliedLast.rawValue)!
-            let inputImage = CGImage ?? CIContext().createCGImage(CIImage!, fromRect: CIImage!.extent)
-            
-            // Render to bitmap.
-            CGContextDrawImage(context, CGRect(x: 0, y: 0, width: 1, height: 1), inputImage)
-        }
+        }*/
+        
+        // Create 1x1 context that interpolates pixels when drawing to it.
+        let context = CGBitmapContextCreate(&bitmap, 1, 1, 8, 4, CGColorSpaceCreateDeviceRGB(), CGBitmapInfo.ByteOrderDefault.rawValue | CGImageAlphaInfo.PremultipliedLast.rawValue)!
+        let inputImage = CGImage ?? CIContext().createCGImage(CIImage!, fromRect: CIImage!.extent)
+        
+        // Render to bitmap.
+        CGContextDrawImage(context, CGRect(x: 0, y: 0, width: 1, height: 1), inputImage)
         
         // Compute result.
         let result = UIColor(red: CGFloat(bitmap[0]) / 255.0, green: CGFloat(bitmap[1]) / 255.0, blue: CGFloat(bitmap[2]) / 255.0, alpha: CGFloat(bitmap[3]) / 255.0)
--- Makestagram/ViewControllers/Drawing/DrawingViewController.swift
+++ Makestagram/ViewControllers/Drawing/DrawingViewController.swift
@@ -12,6 +12,11 @@ import Color_Picker_for_iOS
 typealias SaveCallback = UIImage -> Void
 
 class DrawingViewController: UIViewController {
+    
+    struct RingAndButton {
+        weak var button: UIButton?
+        weak var ringImageView: UIImageView?
+    }
 
     @IBOutlet weak var drawingView: SmoothedBIView!
     var image: UIImage?
@@ -19,18 +24,22 @@ class DrawingViewController: UIViewController {
     @IBOutlet weak var colorButton: UIButton!
     @IBOutlet var customColorButtons: [UIButton]!
     @IBOutlet weak var baseImageView: UIImageView!
+    @IBOutlet weak var moreColorsButton: UIButton!
+    
     var saveCallback: SaveCallback?
+    var ringImageView: UIImageView!
+    var ringImagePair: RingAndButton = RingAndButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let currentTime = NSDate().timeIntervalSince1970
 
-        // Do any additional setup after loading the view.
         baseImageView.image = self.image
         drawingView.currentColor = self.image!.areaAverage()
         let newImage = colorButton.imageView?.image?.imageWithRenderingMode(.AlwaysTemplate)
         colorButton.setImage(newImage, forState: .Normal)
         colorButton.tintColor = drawingView.currentColor
+        
 
         var colorsArray = [UIColor]()
         colorsArray.append(UIColor(colorLiteralRed: 134/255.0, green: 103/255.0, blue: 172/255.0, alpha: 1.0))
@@ -46,12 +55,31 @@ class DrawingViewController: UIViewController {
         
         let duration = NSDate().timeIntervalSince1970 - currentTime
         print("it takes \(duration)")
+        
+        self.ringImageView = UIImageView(frame: colorButton.frame)
+        self.ringImageView.image = UIImage(named: "ring")
+        self.view.addSubview(self.ringImageView)
+        
+        self.ringImagePair.button = colorButton
+        self.ringImagePair.ringImageView = self.ringImageView
+        let newBackImage = moreColorsButton.backgroundImageForState(.Selected)?.imageWithRenderingMode(.AlwaysTemplate)
+        moreColorsButton.setBackgroundImage(newBackImage, forState: .Selected)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        if let bCenter = self.ringImagePair.button?.center {
+            self.ringImagePair.ringImageView?.center = bCenter
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: Saving
     
     func saveImage() -> UIImage {
         UIGraphicsBeginImageContextWithOptions(drawingView.bounds.size, false, 0.0)
@@ -62,6 +90,8 @@ class DrawingViewController: UIViewController {
         return resultImage
     }
     
+    //MARK: Actions
+    
     @IBAction func backButtonTapped(sender: UIButton) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
@@ -69,17 +99,12 @@ class DrawingViewController: UIViewController {
     @IBAction func simpleColorButtonTapped(sender: UIButton) {
         self.drawingView.currentColor = sender.tintColor
         self.drawingView.lineWidth = 2.0
+        self.ringImagePair.button = sender
+        self.ringImageView.center = sender.center
+        moreColorsButton.selected = false
     }
 
-    @IBAction func colorsButtonTapped(sender: UIButton) {
-        let colorPickerController = NEOColorPickerViewController()
-        colorPickerController.delegate = self
-        colorPickerController.selectedColor = drawingView.currentColor
-        colorPickerController.title = NSLocalizedString("Pick a color!", comment: "Picking color title")
-        
-        let navigationController = UINavigationController(rootViewController: colorPickerController)
-        self.presentViewController(navigationController, animated: true, completion: nil)
-        
+    @IBAction func moreColorsButtonTapped(sender: UIButton) {
     }
     
     @IBAction func eraserButtonTapped(sender: AnyObject) {
@@ -112,23 +137,16 @@ class DrawingViewController: UIViewController {
 
 }
 
-extension DrawingViewController: NEOColorPickerViewControllerDelegate {
-    func colorPickerViewController(controller: NEOColorPickerBaseViewController!, didSelectColor color: UIColor!) {
-        self.drawingView.currentColor = color
-        self.drawingView.lineWidth = 2.0
-
-        controller.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func colorPickerViewControllerDidCancel(controller: NEOColorPickerBaseViewController!) {
-        controller.dismissViewControllerAnimated(true, completion: nil)
-
-    }
-}
+//MARK: extension ColorPickerDelegate
 
 extension DrawingViewController: ColorPickerDelegate {
     func colorViewController(controller: UIViewController, didSelectColor: UIColor) {
         self.drawingView.currentColor = didSelectColor
+        moreColorsButton.selected = true
+        moreColorsButton.tintColor = didSelectColor
+        
+        self.ringImagePair.button = moreColorsButton
+        self.ringImageView.center = moreColorsButton.center
     }
     
     func colorViewControllerDidCancel(controller: UIViewController) {
