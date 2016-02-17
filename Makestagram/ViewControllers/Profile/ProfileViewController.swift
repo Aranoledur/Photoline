baa2bfa5e7543d0b122ec8480b4267d87aec6fff
Nikolay Ischuk <ischuknn@gmail.com>
2016-02-17 14:46:13 +0300

    iTunes connect stuff

---
+++ Makestagram/ViewControllers/Profile/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  Photoline
+//
+//  Created by Nikolay Ischuk on 17.02.16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import Foundation
+
+class ProfileViewController: UIViewController {
+    
+}
